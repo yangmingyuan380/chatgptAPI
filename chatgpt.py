1ed1bebc8dff4a51e3098cc5eebc30d63c711056
jetaime <13618318362@163.com>
2023-03-03 19:13:46 +0800

    增加上下文对话功能

--- chatgpt.py
+++ chatgpt.py
@@ -1,13 +1,26 @@
 import openai
 from config import config
+from loguru import logger
 openai.api_key = config['openai']['api_key']
-def sendMsg(text:str):
+chat_history = []
+
+
+def sendMsg(text: str):
+    global chat_history # 声明全局变量
     try:
+        chat_history.append("You: " + text)
+        chat_history=chat_history[-10:]
+        prompt="\n".join(chat_history)
         completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=[{"role": "user", "content": text}]
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a friendly and helpful teaching assistant. You explain concepts in great depth using simple terms, and you give examples to help people learn. At the end of each explanation, you ask a question to check for understanding"},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0.5,
         )
-        return completion['choices'][0]['message']['content']
+        message = completion['choices'][0]['message']['content']
     except Exception as e:
         raise e
-    
+    chat_history.append("Bot: " + message)
+    return message
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import tg
+from loguru import logger
 if __name__=="__main__":
+    logger.add("my_log_file.log")
     tg.startBot()
